79fb0f76cdca6ac980c780d42e24a52f4984bdeb
Vineone1 <44683938+Vineone1@users.noreply.github.com>
2018-11-08 18:04:46 -0200

    Add files via upload

--- src/main/java/br/edu/ulbra/election/candidate/api/v1/CandidateApi.java
+++ src/main/java/br/edu/ulbra/election/candidate/api/v1/CandidateApi.java
@@ -3,6 +3,7 @@ package br.edu.ulbra.election.candidate.api.v1;
 import br.edu.ulbra.election.candidate.input.v1.CandidateInput;
 import br.edu.ulbra.election.candidate.output.v1.CandidateOutput;
 import br.edu.ulbra.election.candidate.output.v1.GenericOutput;
+import br.edu.ulbra.election.candidate.service.CandidateService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,33 +14,40 @@ import java.util.List;
 @RequestMapping("/v1/candidate")
 public class CandidateApi {
 
+    private final CandidateService candidateService;
+    private ArrayList<CandidateOutput> listaRetorno = new ArrayList<CandidateOutput>();
+
+    public CandidateApi(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
     @GetMapping("/")
     @ApiOperation(value = "Get candidates List")
     public List<CandidateOutput> getAll(){
-        return new ArrayList<>();
+        return listaRetorno;
     }
 
     @GetMapping("/{candidateId}")
     @ApiOperation(value = "Get candidate by Id")
     public CandidateOutput getById(@PathVariable Long candidateId){
-        return new CandidateOutput();
+        return candidateService.getById(candidateId);
     }
 
     @PutMapping("/")
     @ApiOperation(value = "Create new candidate")
     public CandidateOutput create(@RequestBody CandidateInput candidateInput){
-        return new CandidateOutput();
+        return candidateService.create(candidateInput);
     }
 
     @PostMapping("/{candidateId}")
     @ApiOperation(value = "Update candidate")
     public CandidateOutput update(@PathVariable Long candidateId, @RequestBody CandidateInput candidateInput){
-        return new CandidateOutput();
+        return candidateService.update(candidateId, candidateInput);
     }
 
     @DeleteMapping("/{candidateId}")
     @ApiOperation(value = "Delete candidate")
     public GenericOutput delete(@PathVariable Long candidateId){
-        return new GenericOutput("OK");
+        return candidateService.delete(candidateId);
     }
 }
